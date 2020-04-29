890c094a59dab7134042f2e449eda4965e258de6
modeverv <modeverv@gmail.com>
2020-04-29 16:58:57 +0900

    3 files modified, 3 added

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,17 +2,20 @@ export const products = [
   {
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    image_url: 'https://pbs.twimg.com/media/EWwXPI-XsAEAa93?format=jpg&name=large'
   },
   {
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    image_url: 'https://pbs.twimg.com/media/EWwXPI-XsAEAa93?format=jpg&name=large'
   },
   {
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: '',
+    image_url: 'https://pbs.twimg.com/media/EWwXPI-XsAEAa93?format=jpg&name=large'
   }
 ];
 
